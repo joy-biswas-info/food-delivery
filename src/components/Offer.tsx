d2e4c3415528313618ddabc71a72178be9a44a09
Joy Biswas <contact.joybiswas@gmail.com>
2023-07-25 17:39:31 +0600

    Improvement On design

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -4,9 +4,9 @@ import CountDown from './CountDown';
 
 const Offer = () => {
     return (
-        <div className='bg-black h-screen flex flex-col p-4 md:flex-row justify-center items-center'>
+        <div className='bg-black h-screen md:h-[70vh] bg-[url("/offerBg.png")]  flex flex-col p-4 my-4 md:flex-row justify-center items-center'>
             {/* Text  */}
-            <div className=' flex-1 flex flex-col items-center'>
+            <div className='flex-1 flex flex-col items-center'>
                 <h1 className='text-2xl md:text-4xl p-2 md:p-4 font-bold text-white text-center'>Consectetur adipisicing elit.</h1>
                 <p className='text-white text-center p-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                     Dolores excepturi ex earum? Natus beatae ex temporibus dolorum </p>
